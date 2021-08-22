e787cee6749f2c31822ca217149fd0e6a380d6b3
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-22 18:24:48 +0400

    Update CakeStats.tsx

--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -5,8 +5,8 @@ import { getBalanceNumber } from 'utils/formatBalance'
 import { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
 import { useTranslation } from 'contexts/Localization'
 import { getCakeAddress } from 'utils/addressHelpers'
-import CardValue from './CardValue'
 import { usePriceCakeBusd } from 'state/farms/hooks'
+import CardValue from './CardValue'
 
 const StyledCakeStats = styled(Card)`
   margin-left: auto;
