feba568b7ce5c0b53d24693a8a40df6edb71b774
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-22 17:13:22 +0400

    Update index.ts

--- src/config/constants/multicall/index.ts
+++ src/config/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@shibcakeswap/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb',
+  [ChainId.MAINNET]: '0xdDf4C84be35E6cd0a14a19443e840ddB90F1ffb3',
   [ChainId.TESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A',
 }
 
