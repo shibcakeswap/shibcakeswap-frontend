ba25697da6778a2d26c06ceda72e7f999bcb9acc
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-22 22:13:55 +0400

    Update types.ts

--- src/config/constants/types.ts
+++ src/config/constants/types.ts
@@ -44,6 +44,15 @@ export interface Ifo {
   [PoolIds.poolUnlimited]: IfoPoolInfo
 }
 
+export enum QuoteToken {
+  'BNB' = 'BNB',
+  'CAKE' = 'CAKE',
+  'SYRUP' = 'SYRUP',
+  'BUSD' = 'BUSD',
+  'TWT' = 'TWT',
+  'UST' = 'UST',
+}
+
 export enum PoolCategory {
   'COMMUNITY' = 'Community',
   'CORE' = 'Core',
