7f2b34bd299c45d6b370656d857de7ed851252a0
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-24 00:58:39 +0400

    Update api.ts

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
 
-
 /*
  * Due to Cors the api was forked and a proxy was created
- * @see https://github.com/pancakeswap/gatsby-pancake-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
+ * @see https://github.com/lydiaswap/gatsby-lydia-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
  */
-export const baseUrl = 'https://api.pancakeswap.com/api/v1'
+export const baseUrl = 'https://api.lydia.finance/api/v1'
 
 /* eslint-disable camelcase */
 
@@ -22,16 +21,21 @@ export const useGetStats = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${baseUrl}/tvl`)
-        const responseData: ApiTvlResponse = await response.json()
+        const response = await fetch(`${baseUrl}/stat`)
+        const responsedata: ApiTvlResponse = await response.json()
 
-        setData(responseData)
+        setData(responsedata)
       } catch (error) {
         console.error('Unable to fetch data:', error)
       }
     }
 
     fetchData()
+
+    const interval = setInterval(() => {
+      fetchData()
+    }, 10000)
+    return () => clearInterval(interval)
   }, [setData])
 
   return data
