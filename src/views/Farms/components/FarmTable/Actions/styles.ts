24f90765d19837bb61de235148472a47ea74b898
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-24 02:23:16 +0400

    Update styles.ts

--- src/views/Farms/components/FarmTable/Actions/styles.ts
+++ src/views/Farms/components/FarmTable/Actions/styles.ts
@@ -7,14 +7,12 @@ export const ActionContainer = styled.div`
   flex-grow: 1;
   flex-basis: 0;
   margin-bottom: 16px;
-
   ${({ theme }) => theme.mediaQueries.sm} {
     margin-left: 12px;
     margin-right: 12px;
     margin-bottom: 0;
     max-height: 100px;
   }
-
   ${({ theme }) => theme.mediaQueries.xl} {
     margin-left: 48px;
     margin-right: 0;
@@ -31,9 +29,4 @@ export const ActionContent = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
-export const Earned = styled.div`
-  font-weight: 600;
-  font-size: 20px;
-  color: ${({ theme }) => theme.colors.text};
 `
