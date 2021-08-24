6f99d6f17aa1193b9d4f19f49648beadaf4c483f
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-24 18:51:18 +0400

    Update styles.ts

--- src/views/Pools/components/PoolsTable/ActionPanel/styles.ts
+++ src/views/Pools/components/PoolsTable/ActionPanel/styles.ts
@@ -1,27 +1,22 @@
 import styled from 'styled-components'
 
-export const ActionContainer = styled.div`
+export const ActionContainer = styled.div<{ isAutoVault?: boolean }>`
   padding: 16px;
   border: 2px solid ${({ theme }) => theme.colors.input};
   border-radius: 16px;
   flex-grow: 1;
   flex-basis: 0;
   margin-bottom: 16px;
-
   ${({ theme }) => theme.mediaQueries.sm} {
     margin-left: 12px;
     margin-right: 12px;
     margin-bottom: 0;
-    height: 130px;
-    max-height: 130px;
+    height: ${({ isAutoVault }) => (isAutoVault ? '130px' : 'auto')};
   }
-
+}
   ${({ theme }) => theme.mediaQueries.xl} {
     margin-left: 32px;
     margin-right: 0;
-    margin-bottom: 0;
-    height: 130px;
-    max-height: 130px;
   }
 `
 
