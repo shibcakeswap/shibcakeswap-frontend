aa400b6b0500bc24482fb460c7e07888133fefac
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-26 15:40:06 +0400

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -12,10 +12,24 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '1',
+    tokenPerBlock: '2.5',
     sortOrder: 1,
     isFinished: false,
     enableEmergencyWithdraw: true,
+  },
+  {
+    sousId: 1,
+    stakingToken: tokens.shibcake,
+    earningToken: tokens.cake,
+    contractAddress: {
+      97: '0x1Dc5685088D038CCe7B826BB7688142c7b5c6DeC',
+      56: '0x1Dc5685088D038CCe7B826BB7688142c7b5c6DeC',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '1',
+    sortOrder: 2,
+    isFinished: false,
   },
 ]
 
