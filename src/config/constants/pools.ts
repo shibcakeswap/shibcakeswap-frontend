771a9a1058f16b4b665ec0441e16d277fcf107e9
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-28 01:40:06 +0400

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -12,7 +12,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '10',
+    tokenPerBlock: '1',
     sortOrder: 1,
     isFinished: false,
     enableEmergencyWithdraw: true,
