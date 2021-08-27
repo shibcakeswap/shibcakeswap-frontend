0f3b56f0289084b5c01f1e88921f79132fc59a16
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-27 21:10:02 +0400

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -29,6 +29,10 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
     label: t('Farms'),
     icon: 'FarmIcon',
     href: '/farms',
+    status: {
+      text: 'NEW POOLS',
+      color: 'success',
+    },
   },
  {
     label: t('Pools'),
