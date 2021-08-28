ccbf48389816c59072e4c853fd32568612c63097
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-29 02:08:37 +0400

    Update useUnstakePool.ts

--- src/views/Pools/hooks/useUnstakePool.ts
+++ src/views/Pools/hooks/useUnstakePool.ts
@@ -1,19 +1,14 @@
 import { useCallback } from 'react'
 import { useWeb3React } from '@web3-react/core'
+import BigNumber from 'bignumber.js'
 import { useAppDispatch } from 'state'
 import { updateUserStakedBalance, updateUserBalance, updateUserPendingReward } from 'state/actions'
 import { unstakeFarm } from 'utils/calls'
-import BigNumber from 'bignumber.js'
-import { DEFAULT_GAS_LIMIT } from 'config'
-import { BIG_TEN } from 'utils/bigNumber'
 import { useMasterchef, useSousChef } from 'hooks/useContract'
+import { BIG_TEN } from 'utils/bigNumber'
 
-const options = {
-  gasLimit: DEFAULT_GAS_LIMIT,
-}
-
-const sousUnstake = async (sousChefContract, amount, decimals = 18) => {
-  const tx = await sousChefContract.withdraw(new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString(), options)
+const sousUnstake = async (sousChefContract, amount, decimals) => {
+  const tx = await sousChefContract.withdraw(new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString())
   const receipt = await tx.wait()
   return receipt.status
 }
@@ -24,7 +19,7 @@ const sousEmergencyUnstake = async (sousChefContract) => {
   return receipt.status
 }
 
-const useUnstakePool = (sousId: number, enableEmergencyWithdraw = true) => {
+const useUnstakePool = (sousId, enableEmergencyWithdraw = true) => {
   const dispatch = useAppDispatch()
   const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
@@ -33,11 +28,11 @@ const useUnstakePool = (sousId: number, enableEmergencyWithdraw = true) => {
   const handleUnstake = useCallback(
     async (amount: string, decimals: number) => {
       if (sousId === 0) {
-        await unstakeFarm(masterChefContract, 0, amount, account)
+        await unstakeFarm(masterChefContract, 0, amount)
       } else if (enableEmergencyWithdraw) {
-        await sousEmergencyUnstake(sousChefContract, amount, account)
+        await sousEmergencyUnstake(sousChefContract)
       } else {
-        await sousUnstake(sousChefContract, amount, account)
+        await sousUnstake(sousChefContract, amount, decimals)
       }
       dispatch(updateUserStakedBalance(sousId, account))
       dispatch(updateUserBalance(sousId, account))
