578cb0b9d4e6311d207b9b8fccb590dad4877c77
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-28 23:26:40 +0400

    Update TotalValueLockedCard.tsx

--- src/views/Home/components/TotalValueLockedCard.tsx
+++ src/views/Home/components/TotalValueLockedCard.tsx
@@ -32,7 +32,7 @@ const TotalValueLockedCard = () => {
         {data ? (
           <>
             <Heading scale="xl">{`$${tvl}`}</Heading>
-            <Text color="textSubtle">{t('Across all LPs and Syrup Pools')}</Text>
+            <Text color="textSubtle">{t('Across all LPs and Electrum Pools')}</Text>
           </>
         ) : (
           <>
