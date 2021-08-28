f99faa389caaa59458be6ebaa4c9f4d28453fd9f
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-29 01:15:49 +0400

    Update fetchPools.ts

--- src/state/pools/fetchPools.ts
+++ src/state/pools/fetchPools.ts
@@ -1,12 +1,10 @@
+import BigNumber from 'bignumber.js'
 import poolsConfig from 'config/constants/pools'
 import sousChefABI from 'config/abi/sousChef.json'
 import cakeABI from 'config/abi/cake.json'
 import wbnbABI from 'config/abi/weth.json'
 import multicall from 'utils/multicall'
-import { getAddress, getMasterChefAddress, getWbnbAddress } from 'utils/addressHelpers'
-import BigNumber from 'bignumber.js'
-import masterchefABI from 'config/abi/masterchef.json'
-
+import { getAddress, getWbnbAddress } from 'utils/addressHelpers'
 import { BIG_ZERO } from 'utils/bigNumber'
 import { getSouschefV2Contract } from 'utils/contractHelpers'
 
@@ -35,29 +33,6 @@ export const fetchPoolsBlockLimits = async () => {
       sousId: cakePoolConfig.sousId,
       startBlock: new BigNumber(startBlock).toJSON(),
       endBlock: new BigNumber(endBlock).toJSON(),
-    }
-  })
-}
-
-export const fetchPools = async () => {
-  const nonBnbPools = poolsConfig.filter((p) => p.stakingToken.symbol !== 'BNB')
-  const calls = nonBnbPools.map((poolConfig) => {
-    return {
-      address: getMasterChefAddress(),
-      name: 'poolInfo',
-      params: [poolConfig.sousId],
-    }
-  })
-  const poolsInfo = await multicall(masterchefABI, calls)
-  return nonBnbPools.map((p, index) => {
-    const info = poolsInfo[index]
-
-    const harvestInterval = new BigNumber(info.harvestInterval._hex)
-    const depositFeeBP = new BigNumber(info.depositFeeBP)
-    return {
-      sousId: p.sousId,
-      depositFeeBP: depositFeeBP.div(100).toString(),
-      harvestInterval: harvestInterval.div(60).div(60).toString(),
     }
   })
 }
@@ -97,32 +72,32 @@ export const fetchPoolsTotalStaking = async () => {
   ]
 }
 
-export const fetchPoolStakingLimit = async (sousId: number): Promise<BigNumber> => {	
-  try {	
-    const sousContract = getSouschefV2Contract(sousId)	
-    const stakingLimit = await sousContract.poolLimitPerUser()	
-    return new BigNumber(stakingLimit.toString())	
-  } catch (error) {	
-    return BIG_ZERO	
-  }	
-}	
+export const fetchPoolStakingLimit = async (sousId: number): Promise<BigNumber> => {
+  try {
+    const sousContract = getSouschefV2Contract(sousId)
+    const stakingLimit = await sousContract.poolLimitPerUser()
+    return new BigNumber(stakingLimit.toString())
+  } catch (error) {
+    return BIG_ZERO
+  }
+}
 
-export const fetchPoolsStakingLimits = async (	
-  poolsWithStakingLimit: number[],	
-): Promise<{ [key: string]: BigNumber }> => {	
-  const validPools = poolsConfig	
-    .filter((p) => p.stakingToken.symbol !== 'BNB' && !p.isFinished)	
-    .filter((p) => !poolsWithStakingLimit.includes(p.sousId))	
+export const fetchPoolsStakingLimits = async (
+  poolsWithStakingLimit: number[],
+): Promise<{ [key: string]: BigNumber }> => {
+  const validPools = poolsConfig
+    .filter((p) => p.stakingToken.symbol !== 'BNB' && !p.isFinished)
+    .filter((p) => !poolsWithStakingLimit.includes(p.sousId))
 
-  // Get the staking limit for each valid pool	
-  // Note: We cannot batch the calls via multicall because V1 pools do not have "poolLimitPerUser" and will throw an error	
-  const stakingLimitPromises = validPools.map((validPool) => fetchPoolStakingLimit(validPool.sousId))	
-  const stakingLimits = await Promise.all(stakingLimitPromises)	
+  // Get the staking limit for each valid pool
+  // Note: We cannot batch the calls via multicall because V1 pools do not have "poolLimitPerUser" and will throw an error
+  const stakingLimitPromises = validPools.map((validPool) => fetchPoolStakingLimit(validPool.sousId))
+  const stakingLimits = await Promise.all(stakingLimitPromises)
 
-  return stakingLimits.reduce((accum, stakingLimit, index) => {	
-    return {	
-      ...accum,	
-      [validPools[index].sousId]: stakingLimit,	
-    }	
-  }, {})	
-}	
+  return stakingLimits.reduce((accum, stakingLimit, index) => {
+    return {
+      ...accum,
+      [validPools[index].sousId]: stakingLimit,
+    }
+  }, {})
+}
