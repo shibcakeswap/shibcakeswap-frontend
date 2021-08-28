baa77613d10b19ee228f9bcac75c61dccb0647b4
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-28 22:54:23 +0400

    Update fetchPools.ts

--- src/state/pools/fetchPools.ts
+++ src/state/pools/fetchPools.ts
@@ -1,7 +1,7 @@
 import poolsConfig from 'config/constants/pools'
 import sousChefABI from 'config/abi/sousChef.json'
 import cakeABI from 'config/abi/cake.json'
-import wbnbABI from 'config/abi/wbnb.json'
+import wbnbABI from 'config/abi/weth.json'
 import multicall from 'utils/multicall'
 import { getAddress, getMasterChefAddress, getWbnbAddress } from 'utils/addressHelpers'
 import BigNumber from 'bignumber.js'
