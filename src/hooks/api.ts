50d557387cedca3a379fcc42bf8e574be7afd8b4
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-28 21:18:45 +0400

    Update api.ts

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,4 +1,6 @@
+import BigNumber from 'bignumber.js'
 import { useEffect, useState } from 'react'
+import { getBalanceNumber } from 'utils/formatBalance'
 
 /*
  * Due to Cors the api was forked and a proxy was created
@@ -8,34 +10,51 @@ export const baseUrl = 'https://api-sigma-eight.vercel.app/api'
 
 /* eslint-disable camelcase */
 
-export interface ApiTvlResponse {
+export interface ApiSummaryResponse {
   update_at: string
-  '24h_total_volume': number
-  total_value_locked: number
-  total_value_locked_all: number
+  data: Map<string, Summary>
+}
+
+export interface Summary {
+  liquidity: string
+}
+
+export interface Stats {
+  tvl: number
 }
 
 export const useGetStats = () => {
-  const [data, setData] = useState<ApiTvlResponse | null>(null)
+  const [data, setData] = useState<Stats | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${baseUrl}/stat`)
-        const responsedata: ApiTvlResponse = await response.json()
+        const response = await fetch(`${baseUrl}/summary`)
+        const responsedata: ApiSummaryResponse = await response.json()
 
-        setData(responsedata)
+        const stats: Stats = { tvl: 0 }
+        // eslint-disable-next-line
+        Object.keys(responsedata.data).forEach(function (key) {
+          stats.tvl += parseInt(responsedata.data[key].liquidity)
+        })
+
+        const pools = await fetchPoolsTotalStaking()
+        const cakePrice = parseInt(
+          responsedata.data['0x55d398326f99059fF775485246999027B3197955_0x818CEE824f8CaEAa05Fb6a1f195935e364D52Af0']
+            .price,
+        )
+        pools.forEach((pool) => {
+          const total = getBalanceNumber(new BigNumber(pool.totalStaked), 18) / cakePrice
+          stats.tvl += total
+        })
+
+        setData(stats)
       } catch (error) {
         console.error('Unable to fetch data:', error)
       }
     }
 
     fetchData()
-
-    const interval = setInterval(() => {
-      fetchData()
-    }, 10000)
-    return () => clearInterval(interval)
   }, [setData])
 
   return data
