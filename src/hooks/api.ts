9c905e760350f82209b8ca33d69f5d49694db558
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-28 21:46:53 +0400

    Update api.ts

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -42,7 +42,7 @@ export const useGetStats = () => {
 
         const pools = await fetchPoolsTotalStaking()
         const cakePrice = parseInt(
-          responsedata.data['0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c_0x818CEE824f8CaEAa05Fb6a1f195935e364D52Af0']
+          responsedata.data['0x55d398326f99059fF775485246999027B3197955_0x818CEE824f8CaEAa05Fb6a1f195935e364D52Af0']
             .price,
         )
         pools.forEach((pool) => {
