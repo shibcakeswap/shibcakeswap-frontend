968cd936baf5e59019c5395de8a1f146ad45a37a
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-29 15:53:25 +0400

    Update formatBalance.ts

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -1,6 +1,4 @@
 import BigNumber from 'bignumber.js'
-import { ethers } from 'ethers'
-import { formatUnits } from 'ethers/lib/utils'
 import { BIG_TEN } from './bigNumber'
 
 /**
@@ -31,32 +29,4 @@ export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2)
     maximumFractionDigits: maxPrecision,
   }
   return number.toLocaleString(undefined, options)
-}
-
-/**
- * Method to format the display of wei given an ethers.BigNumber object
- * Note: does NOT round
- */
-export const formatBigNumber = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
-  const remainder = number.mod(ethers.BigNumber.from(10).pow(decimals - displayDecimals))
-  return formatUnits(number.sub(remainder), decimals)
-}
-
-/**
- * Method to format the display of wei given an ethers.BigNumber object with toFixed
- * Note: rounds
- */
-export const formatBigNumberToFixed = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
-  const formattedString = formatUnits(number, decimals)
-  return (+formattedString).toFixed(displayDecimals)
-}
-
-/**
- * Formats a FixedNumber like BigNumber
- * i.e. Formats 9763410526137450427.1196 into 9.763 (3 display decimals)
- */
-export const formatFixedNumber = (number: ethers.FixedNumber, displayDecimals = 18, decimals = 18) => {
-  // Remove decimal
-  const [leftSide] = number.toString().split('.')
-  return formatBigNumber(ethers.BigNumber.from(leftSide), displayDecimals, decimals)
 }
