c0eacce46be7eca5cc244483000fe53b21e5690e
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-30 20:10:02 +0400

    Update hooks.ts

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -1,13 +1,159 @@
+import BigNumber from 'bignumber.js'
 import { useEffect, useMemo } from 'react'
 import { useWeb3React } from '@web3-react/core'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import useRefresh from 'hooks/useRefresh'
+import { getWeb3NoAccount } from 'utils/web3'
 import { ethers } from 'ethers'
 import { minBy, orderBy } from 'lodash'
 import { useAppDispatch } from 'state'
 import Nfts from 'config/constants/nfts'
-import { State, NodeRound, ReduxNodeLedger, NodeLedger, ReduxNodeRound } from './types'
+import { fetchFarmsPublicDataAsync, fetchPoolsUserDataAsync, fetchPoolsPublicDataAsync, setBlock } from './actions'
+import { State, Farm, Pool, Block, PriceState, NodeRound, ReduxNodeLedger, NodeLedger, ReduxNodeRound } from './types'
+import { QuoteToken } from '../config/constants/types'
+import { fetchPrices } from './prices'
 import { fetchWalletNfts } from './collectibles'
 import { parseBigNumberObj } from './predictions/helpers'
+
+const ZERO = new BigNumber(0)
+
+export const useFetchPublicData = () => {
+  const dispatch = useDispatch()
+  const { slowRefresh } = useRefresh()
+  
+  useEffect(() => {
+    dispatch(fetchFarmsPublicDataAsync())
+    dispatch(fetchPoolsPublicDataAsync())
+  }, [dispatch, slowRefresh])
+
+  useEffect(() => {
+    const web3 = getWeb3NoAccount()
+    const interval = setInterval(async () => {
+      const blockNumber = await web3.eth.getBlockNumber()
+      dispatch(setBlock(blockNumber))
+    }, 6000)
+
+    return () => clearInterval(interval)
+  }, [dispatch])
+}
+
+// Farms
+
+export const useFarms = (): Farm[] => {
+  const farms = useSelector((state: State) => state.farms.data)
+  return farms
+}
+
+export const useFarmFromPid = (pid): Farm => {
+  const farm = useSelector((state: State) => state.farms.data.find((f) => f.pid === pid))
+  return farm
+}
+
+export const useFarmFromSymbol = (lpSymbol: string): Farm => {
+  const farm = useSelector((state: State) => state.farms.data.find((f) => f.lpSymbol === lpSymbol))
+  return farm
+}
+
+export const useFarmUser = (pid) => {
+  const farm = useFarmFromPid(pid)
+
+  return {
+    allowance: farm.userData ? new BigNumber(farm.userData.allowance) : new BigNumber(0),
+    tokenBalance: farm.userData ? new BigNumber(farm.userData.tokenBalance) : new BigNumber(0),
+    stakedBalance: farm.userData ? new BigNumber(farm.userData.stakedBalance) : new BigNumber(0),
+    earnings: farm.userData ? new BigNumber(farm.userData.earnings) : new BigNumber(0),
+  }
+}
+
+// Pools
+
+export const usePools = (account): Pool[] => {
+  const { fastRefresh } = useRefresh()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (account) {
+      dispatch(fetchPoolsUserDataAsync(account))
+    }
+  }, [account, dispatch, fastRefresh])
+
+  const pools = useSelector((state: State) => state.pools.data)
+  return pools
+}
+
+export const usePoolFromPid = (sousId): Pool => {
+  const pool = useSelector((state: State) => state.pools.data.find((p) => p.sousId === sousId))
+  return pool
+}
+
+// Prices
+
+export const usePriceBnbBusd = (): BigNumber => {
+  const pid = 3 // BNB-BUSD LP
+  const farm = useFarmFromPid(pid)
+  return farm.tokenPriceVsQuote ? new BigNumber(farm.tokenPriceVsQuote) : ZERO
+}
+
+export const usePriceCakeBusd = (): BigNumber => {
+  const pid = 1 // CAKE-BNB LP
+  const farm = useFarmFromPid(pid)
+  return farm.tokenPriceVsQuote ? new BigNumber(farm.tokenPriceVsQuote) : ZERO
+}
+
+export const useTotalValue = (): BigNumber => {
+  const farms = useFarms()
+  const bnbPrice = usePriceBnbBusd()
+  const cakePrice = usePriceCakeBusd()
+  let value = new BigNumber(0)
+  for (let i = 0; i < farms.length; i++) {
+    const farm = farms[i]
+    if (farm.lpTotalInQuoteToken) {
+      let val
+      if (farm.quoteTokenSymbol === QuoteToken.BNB) {
+        val = bnbPrice.times(farm.lpTotalInQuoteToken)
+      } else if (farm.quoteTokenSymbol === QuoteToken.CAKE) {
+        val = cakePrice.times(farm.lpTotalInQuoteToken)
+      } else {
+        val = farm.lpTotalInQuoteToken
+      }
+      value = value.plus(val)
+    }
+  }
+  return value
+}
+
+// Prices
+export const useFetchPriceList = () => {
+  const { slowRefresh } = useRefresh()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchPrices())
+  }, [dispatch, slowRefresh])
+}
+
+export const useGetApiPrices = () => {
+  const prices: PriceState['data'] = useSelector((state: State) => state.prices.data)
+  return prices
+}
+
+export const useGetApiPrice = (token: string) => {
+  const prices = useGetApiPrices()
+  let tokenSymbol = token.toLowerCase()
+
+  if (!prices) {
+    return null
+  }
+
+  if (tokenSymbol === 'bnb') tokenSymbol = 'wbnb'
+  if (tokenSymbol === 'btc') tokenSymbol = 'btcb'
+
+  return prices[tokenSymbol]
+}
+
+// Block
+export const useBlock = (): Block => {
+  return useSelector((state: State) => state.block)
+}
 
 // /!\
 // Don't add anything here. These hooks will be moved the the predictions folder
