b96d41c69775517c92ae2bd19c527eb8b993b088
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-31 10:59:19 +0400

    Update index.tsx

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -99,7 +99,7 @@ const Pools: React.FC = () => {
 
   const pools = useMemo(() => {
     const cakePool = poolsWithoutAutoVault.find((pool) => pool.sousId === 0)
-    const cakeAutoVault = { ...cakePool, isAutoVault: true }
+    const cakeAutoVault = { ...cakePool, isAutoVault: false }
     return [cakeAutoVault, ...poolsWithoutAutoVault]
   }, [poolsWithoutAutoVault])
 
