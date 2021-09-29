e765f376c3b51bd8ecc062ab4b0e9f8970ae9e81
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-09-29 06:21:23 +0400

    Update ShibcakeWarning.tsx

--- src/views/Swap/components/SwapWarningModal/ShibcakeWarning.tsx
+++ src/views/Swap/components/SwapWarningModal/ShibcakeWarning.tsx
@@ -9,9 +9,9 @@ const ShibcakeWarning = () => {
     <>
       <Text>{t('To trade SHIBCAKE, you must:')} </Text>
       <Text>• {t('Click on the settings icon')}</Text>
-      <Text mb="24px">• {t('Set your slippage tolerance to 26%+')}</Text>
-      <Text>{t('This is because ShibCake taxes a 20% (BUY) and 21% (SELL) fee on each transaction:')}</Text>
-      <Text>• {t('15% fee = redistributed in PancakeSwap CAKE to all existing holders')}</Text>
+      <Text mb="24px">• {t('Set your slippage tolerance to 12%+')}</Text>
+      <Text>{t('This is because ShibCake taxes a 10% (BUY) and 11% (SELL) fee on each transaction:')}</Text>
+      <Text>• {t('5% fee = redistributed in PancakeSwap CAKE to all existing holders')}</Text>
       <Text>• {t('4% fee = to the marketing wallet')}</Text>
       <Text>• {t('2% fee = used to add liquidity')}</Text>
     </>
